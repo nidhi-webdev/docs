1edcbc242dd40eb0f6cb9cd1279d62942e6c8837
nidhi <kumarinidhi0594@gmail.com>
2025-10-08 17:38:05 +0200

    New Component

---
+++ src/Components/backGround.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const backGround = () => {
+    return (
+        <>
+            <div className='absolute top-[5%] bg-sky-200 w-full p-10 flex justify-center text-zinc-600 text-xl font-semibold'> Documents.</div>
+            <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tight font-semibold text-zinc-400'> Docs. </h1>
+        </>
+
+    )
+}
+
+export default backGround
