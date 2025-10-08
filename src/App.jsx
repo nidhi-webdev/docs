d1c7aa679e7627c54d878f3845d539d7e1799e19
nidhi <kumarinidhi0594@gmail.com>
2025-10-08 18:41:51 +0200

    For Ground

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import BackGround from './Components/BackGround'
+import ForGround from './Components/ForGround'
 
 const App = () => {
   return (
     <div className='relative full-width h-screen bg-zinc-800'>
       <BackGround />
-      <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
-
-      </div>
+      <ForGround />
     </div>
   )
 }
---
+++ src/Components/ForGround.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ForGround() {
+  return (
+   <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
+
+      </div>
+  )
+}
+
+export default ForGround
