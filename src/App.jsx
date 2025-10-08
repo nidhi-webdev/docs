961b0db1295cc65bed012c7a7743fe4fd67a3166
nidhi <kumarinidhi0594@gmail.com>
2025-10-08 17:08:57 +0200

    Trying with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-2xl text-red-700'>
-      New project Starting with tailwind
+    <div className='relative full-width h-screen bg-zinc-800'>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] text-[13vw] leading-none tracking-tight'> Docs </h1>
     </div>
   )
 }
