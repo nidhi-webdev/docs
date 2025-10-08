08a6b3bfc1c45ff70de29498d230b7c5fb2d2e9e
nidhi <kumarinidhi0594@gmail.com>
2025-10-08 17:15:06 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='relative full-width h-screen bg-zinc-800'>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] text-[13vw] leading-none tracking-tight'> Docs </h1>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tight font-semibold text-zinc-400'> Docs. </h1>
     </div>
   )
 }
