9dadc420a9bf776256898f1d2c67c501d6ca965c
nidhi <kumarinidhi0594@gmail.com>
2025-10-08 18:41:25 +0200

    Commiting

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   return (
     <div className='relative full-width h-screen bg-zinc-800'>
       <BackGround />
+      <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
+
+      </div>
     </div>
   )
 }
--- src/Components/backGround.jsx
+++ src/Components/backGround.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const BackGround = () => {
     return (
         <>
-            <div className='absolute top-[5%] bg-sky-200 w-full p-10 flex justify-center text-zinc-600 text-xl font-semibold'> Documents.</div>
+        <div className='fixed z-[2] w-full h-screen'>
+            <div className='absolute top-[5%] w-full p-10 flex justify-center text-zinc-600 text-xl font-semibold'> Documents.</div>
             <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tight font-semibold text-zinc-400'> Docs. </h1>
+        </div>
+            
         </>
 
     )
