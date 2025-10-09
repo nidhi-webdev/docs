d6eb4bf1a845831344507ce7dee754284feebdeb
nidhi <kumarinidhi0594@gmail.com>
2025-10-09 18:42:27 +0200

    Card

--- src/Components/ForGround.jsx
+++ src/Components/ForGround.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Card from './Card'
 
 function ForGround() {
     return (
         <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
+            <Card />
         </div>
     )
 }
