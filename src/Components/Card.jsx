520689287934c93166f1f49e4ba33f13f8051c9b
nidhi <kumarinidhi0594@gmail.com>
2025-10-09 18:54:54 +0200

    card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 
 const Card = () => {
-  return (
-    <>
-     <div className='w-60 h-72 bg-sky-200 rounded-2xl p-5'>
-        <FaRegFileAlt />
-
-         </div>
-    </>
-  )
+    return (
+        <>
+            <div className='w-60 h-72 bg-sky-200 rounded-2xl p-5 relative'>
+                <FaRegFileAlt />
+                <p className='text-sm mt-5 font-semibold'> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <div className=''> Footer </div>
+            </div>
+        </>
+    )
 }
 
 export default Card
