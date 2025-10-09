d02b58c41ffe113f5b74599aa97ce1fb3972c2aa
nidhi <kumarinidhi0594@gmail.com>
2025-10-09 18:40:55 +0200

    Card

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+     <div className='w-60 h-72 bg-sky-200 rounded-2xl'> </div>
+    </div>
+  )
+}
+
+export default Card
--- src/Components/ForGround.jsx
+++ src/Components/ForGround.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 function ForGround() {
-  return (
-   <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
-
-      </div>
-  )
+    return (
+        <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
+        </div>
+    )
 }
 
 export default ForGround
