113f22e7c14a0b952ac9283eeac620876bb39688
nidhi <kumarinidhi0594@gmail.com>
2025-10-09 18:47:07 +0200

    React Icon

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
 
 const Card = () => {
   return (
-    <div>
-     <div className='w-60 h-72 bg-sky-200 rounded-2xl'> </div>
-    </div>
+    <>
+     <div className='w-60 h-72 bg-sky-200 rounded-2xl p-5'>
+        <FaRegFileAlt />
+
+         </div>
+    </>
   )
 }
 
