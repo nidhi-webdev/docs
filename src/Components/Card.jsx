5010af2b802afea114730176ce1614d72b632629
nidhi <kumarinidhi0594@gmail.com>
2025-10-11 14:39:43 +0200

    card Footer

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,10 +4,10 @@ import { FaRegFileAlt } from "react-icons/fa";
 const Card = () => {
     return (
         <>
-            <div className='w-60 h-72 bg-sky-200 rounded-2xl p-5 relative'>
+            <div className='w-60 h-72 bg-zinc-900 rounded-[50px] px-8 py-10 relative text-white overflow-hidden'>
                 <FaRegFileAlt />
                 <p className='text-sm mt-5 font-semibold'> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                <div className=''> Footer </div>
+                <div className='footer bg-sky-200 bottom-0 absolute w-full h-10 left-0'> Footer </div>
             </div>
         </>
     )
