75edc830505f66ea9b97a5b46302936e75b3a018
nidhi <kumarinidhi0594@gmail.com>
2025-10-11 16:13:57 +0200

    card Footer

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,7 +10,7 @@ const Card = () => {
                 <FaRegFileAlt />
                 <p className='text-sm mt-5 font-semibold'> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
                 <div className='footer  bottom-0 absolute w-full left-0 px-8 py-3'> 
-                    <div className='flex items-center justify-between mb-5'> 
+                    <div className='flex items-center justify-between mb-5 '> 
                         <h5> .4mb </h5>
                         <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
                             <LuDownload size=".8em"color='#fff' />
