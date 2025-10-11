ac933fb5e5688fab1028dfac44b95a16e58d8756
nidhi <kumarinidhi0594@gmail.com>
2025-10-11 15:01:29 +0200

    card Footer

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+
 
 const Card = () => {
     return (
@@ -7,9 +9,10 @@ const Card = () => {
             <div className='w-60 h-72 bg-zinc-900 rounded-[50px] px-8 py-10 relative text-white overflow-hidden'>
                 <FaRegFileAlt />
                 <p className='text-sm mt-5 font-semibold'> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                <div className='footer bg-sky-200 bottom-0 absolute w-full h-10 left-0'> 
-                    <div> 
+                <div className='footer  bottom-0 absolute w-full left-0 px-8 py-3'> 
+                    <div className='flex items-center justify-between mb-5'> 
                         <h5> .4mb </h5>
+                        <LuDownload />
                     </div>
                      </div>
             </div>
