9f29a9a2973bc8870c79eec0e1b74479544a979e
nidhi <kumarinidhi0594@gmail.com>
2025-10-11 16:56:12 +0200

    Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -17,7 +17,7 @@ const Card = () => {
                         </span>
                         
                     </div>
-                    <div className='tag w-full py-4 bg-sky-200'> </div>
+                    <div className='tag w-full py-4 bg-green-600'> Download Now </div>
                      </div>
 
             </div>
