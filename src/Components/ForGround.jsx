5d627826dc398942c26f5f38b8949d9120d5a1b6
nidhi <kumarinidhi0594@gmail.com>
2025-10-12 11:31:50 +0200

    mapping data

--- src/Components/ForGround.jsx
+++ src/Components/ForGround.jsx
@@ -11,7 +11,11 @@ function ForGround() {
     ]
     return (
         <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
-            <Card />
+           
+           {data.map((item, index) => 
+           <Card />
+          
+     )}
         </div>
     )
 }
