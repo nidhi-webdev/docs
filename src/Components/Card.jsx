bdcc5053e828d30a0125827c6b176597a207246e
nidhi <kumarinidhi0594@gmail.com>
2025-10-12 11:42:06 +0200

    Passing data In a correct way

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,16 +4,16 @@ import { LuDownload } from "react-icons/lu";
 
 
 const Card = ({data}) => {
-    console.log("Data from Card" ,data)
+    console.log("Data from Card" ,data.desc)
     
     return (
        
             <div className='w-60 h-72 bg-zinc-900 rounded-[45px] px-8 py-10 relative text-white overflow-hidden'>
                 <FaRegFileAlt />
-                <p className='text-sm mt-5 font-semibold'> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <p className='text-sm mt-5 font-semibold'> {data.desc} </p>
                 <div className='footer  bottom-0 absolute w-full left-0'> 
                     <div className='flex items-center justify-between mb-3 px-8 py-3 '> 
-                        <h5> .4mb </h5>
+                        <h5> {data.filesize} </h5>
                         <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
                             <LuDownload size=".8em"color='#fff' />
                         </span>
--- src/Components/ForGround.jsx
+++ src/Components/ForGround.jsx
@@ -13,7 +13,7 @@ function ForGround() {
         <div className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50'>
            
            {data.map((item, idx) => 
-           <Card key={idx} data = {data}  />
+           <Card key={idx} data = {item}  />
           
      )}
         </div>
