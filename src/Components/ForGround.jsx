d4e3442891f9bf796b630e2b4769b36786a9b5d2
nidhi <kumarinidhi0594@gmail.com>
2025-10-16 19:22:18 +0200

    given nano id

--- src/Components/ForGround.jsx
+++ src/Components/ForGround.jsx
@@ -5,39 +5,39 @@ import { nanoid } from 'nanoid'
 
 
 function ForGround() {
-  // Giving the constraints so that The drable card should not go out side of the ForGround 
-  const ref = useRef(null);
+    // Giving the constraints so that The drable card should not go out side of the ForGround 
+    const ref = useRef(null);
 
-  //converting Static data to state
-  const [cards, setCards] = useState([
-     { 
-        id: nanoid(),
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-          filesize: ".9mb",
-          close: false,
-          tag: {isOpen: true , tagTitle: "Download Now", tagColor: "green" }
-        },
-         { 
+    //converting Static data to state
+    const [cards, setCards] = useState([
+        {
             id: nanoid(),
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-          filesize: ".9mb",
-          close: true,
-          tag: {isOpen: true , tagTitle: "Download Now", tagColor: "sky" }
+            filesize: ".9mb",
+            close: false,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
         },
-         { 
+        {
             id: nanoid(),
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-          filesize: ".9mb",
-          close: false,
-          tag: {isOpen: false , tagTitle: "Download Now", tagColor: "red" }
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "sky" }
+        },
+        {
+            id: nanoid(),
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".9mb",
+            close: false,
+            tag: { isOpen: false, tagTitle: "Download Now", tagColor: "red" }
         }
 
-  ])
+    ])
 
     return (
-        <div ref= {ref} className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50 flex gap-10 flex-wrap p-5'>
-           
-           {/* {data.map((item, idx) => 
+        <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50 flex gap-10 flex-wrap p-5'>
+
+            {/* {data.map((item, idx) => 
            <Card key={idx} data = {item} reference = {ref} />
           
      )} */}
