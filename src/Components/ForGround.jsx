fb9d25fbe9619d8be7a18d2a1844d8fc0ce77b36
nidhi <kumarinidhi0594@gmail.com>
2025-10-17 12:34:36 +0200

    Adding Delete Functinality

--- src/Components/ForGround.jsx
+++ src/Components/ForGround.jsx
@@ -50,6 +50,7 @@ function ForGround() {
         <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full bg-sky-800/50 flex gap-10 flex-wrap p-5'>
             {/* Add button */}
             <button onClick={addCard} className='bg-blue-600 text-white px-4 py2 rounded-2xl fixed top-5 right-5  hover:bg-blue-700 z-10'> + Add Cards </button>
+            <button  className='bg-red-600 text-white  px-4 py2 rounded-2xl fixed  top-5 right-40 hover:bg-red-700 z-10'> - Delete card </button>
 
             {cards.map((item, idx) =>
                 <Card key={idx} data={item} reference={ref} />
