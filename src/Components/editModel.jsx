256bfadece8671c2b5cc2f805f48e04f0e631af7
nidhi <kumarinidhi0594@gmail.com>
2025-10-19 22:16:58 +0200

    The local is coming to the edit input field

--- src/Components/editModel.jsx
+++ src/Components/editModel.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react"
 
 
 const EditModal = ({ isOpen, cardData, onClose }) => {
     console.log("from Edit Model", cardData)
+
+    const [desc, setDesc] = useState('')
+    const [filesize, setFilesize] = useState('')
+    const [tagTitle, setTagTitle] = useState('')
+    const [tagColor, setTagColor] = useState('')
+
+    useEffect(() => {
+      if(isOpen && cardData) {
+        setDesc(cardData.desc || '')
+        setFilesize(cardData.filesize || '')
+      }
+    }, [isOpen, cardData])
+    
 
     if (!isOpen) return null
 
@@ -11,11 +25,18 @@ const EditModal = ({ isOpen, cardData, onClose }) => {
                 <h2 className='text-2xl font-bold mb-4 text-gray-800'> Edit Card </h2>
                 {/* <p className='text-gray-600'> Editing: {cardData?.desc} </p> */}
                 <label> Description </label>
-                <input 
-                className="px-2 py-3 border border-gray-400 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" />
+                <input value={desc} 
+                       onChange={(e) => setDesc(e.target.value)}
+                    className="px-2 py-3 border border-gray-400 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" />
+
+                <label> filesize </label>
+                <input value={filesize}
+                 onChange={(e) => setFilesize(e.target.value)}
+                    className="px-2 py-3 border border-gray-400 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" />
+
                 <button className=''> Save </button>
                 <button onClick={onClose}
-                className='bg-gray-500 text-white px-4 py-2 rounded-2xl hover:bg-gray-600 mt-4 cursor-pointer'> Cancel </button>
+                    className='bg-gray-500 text-white px-4 py-2 rounded-2xl hover:bg-gray-600 mt-4 cursor-pointer'> Cancel </button>
             </div>
         </div>
     )
