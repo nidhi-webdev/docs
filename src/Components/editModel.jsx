b7e17ae64472a042749d397495cbd91b07bad5ae
nidhi <kumarinidhi0594@gmail.com>
2025-10-19 19:20:16 +0200

    trying for edit the card data

--- src/Components/editModel.jsx
+++ src/Components/editModel.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Card from './Card'
 
 const EditModal = ({isOpen, cardData, onClose}) => {
-    
+
    if(!isOpen) return null
 
   return (
     <div>
-    
+    <div className='bg-white rounded-lg p-6 w-96'> 
+<h2> Edit Card </h2>
+    </div>
     </div>
   )
 }
