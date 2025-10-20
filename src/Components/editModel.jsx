674d2bcca9bf34acbb03709a627c8d3c195777d6
nidhi <kumarinidhi0594@gmail.com>
2025-10-20 12:44:31 +0200

    design

--- src/Components/editModel.jsx
+++ src/Components/editModel.jsx
@@ -49,7 +49,7 @@ const EditModal = ({ isOpen, cardData, onClose }) => {
 
 
 
-                <button className='bg-green-600 text-white px-4 py-2 rounded-2xl hover:bg-green-700 cursor-pointer float-start'> Save </button>
+                <button className='bg-green-600 text-white px-6 py-2 rounded-2xl mt-3 hover:bg-green-700 cursor-pointer float-start'> Save </button>
                 <button onClick={onClose}
                     className='bg-gray-500 text-white px-4 py-2 rounded-2xl hover:bg-gray-600 mt-4 cursor-pointer float-end'> Cancel </button>
             </div>
