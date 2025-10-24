7bb78ac27f2687ca094270dc7ab802ee24255795
nidhi <kumarinidhi0594@gmail.com>
2025-10-24 16:39:18 +0200

    Login design

--- docs-frontend/src/Components/login.jsx
+++ docs-frontend/src/Components/login.jsx
@@ -6,10 +6,18 @@ const Login = () => {
     <div className='relative h-screen w-full'>
       <LoginBackGround />
       <div className='absolute inset-0 z-20 flex items-center justify-center  pointer-events-none'>
-        <div className='bg-amber-600 p-6 rounded-2xl shadow-2xl pointer-events-auto'>
-          <label> Username  </label>
-          <input type='text' placeholder='Enter your Username' className='w-5 h-1' />
-        </div>
+        <form className='bg-amber-600 p-6 w-80 rounded-2xl shadow-2xl pointer-events-auto'>
+          <label className='block text-white mb-1'> Username  </label>
+          <input type='text' placeholder='Enter your Username'
+            className='w-65 px-3 py-2 rounded-2xl'
+          />
+
+          <label className='block text-white mb-1'> Password  </label>
+          <input type='text' placeholder='Enter your Password'
+            className='w-65 px-3 py-2 rounded-2xl'
+          />
+
+        </form>
       </div>
 
       <input />
