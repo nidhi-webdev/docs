280ee5df42b2e44566cacd696c22f75d594f0ef3
nidhi <kumarinidhi0594@gmail.com>
2025-10-24 15:51:06 +0200

    Login design

--- docs-frontend/src/Components/LoginBackGround.jsx
+++ docs-frontend/src/Components/LoginBackGround.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const LoginBackGround = () => {
   return (
     <div className='relative h-screen w-full'>
-      <img src='/LoginImage.png'alt="Login" className='h-full w-full' />
-            <div className='absolute inset-0 bg-black/50' />
+
+      <div className='absolute inset-0 bg-black/50' />
+      <img src='/LoginImage.png' alt="Login" className='h-full w-full' />
 
     </div>
   )
--- docs-frontend/src/Components/login.jsx
+++ docs-frontend/src/Components/login.jsx
@@ -3,10 +3,16 @@ import LoginBackGround from '../Components/LoginBackGround'
 
 const Login = () => {
   return (
-    <div>
-        <LoginBackGround />
-     <label>  </label>
-     <input />
+    <div className='relative h-screen w-full'>
+      <LoginBackGround />
+      <div className='absolute inset-0 z-20 flex items-center justify-center  pointer-events-none'>
+        <div className='bg-amber-600 p-6 rounded-2xl shadow-2xl pointer-events-auto'>
+          <label> Username  </label>
+          <input type='text' placeholder='Enter your Username' className='w-5 h-1' />
+        </div>
+      </div>
+
+      <input />
     </div>
   )
 }
