bf9287924b1fe390b72924f759c80896ec834e40
nidhi <kumarinidhi0594@gmail.com>
2025-10-24 12:27:15 +0200

    Login Page

--- docs-frontend/src/App.jsx
+++ docs-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BackGround from './Components/backGround'
 import ForGround from './Components/ForGround'
+import Login from './Components/login'
 
 const App = () => {
   return (
     <div className='relative full-width h-screen bg-zinc-800'>
-      <BackGround />
-      <ForGround />
+      {/* <BackGround />
+      <ForGround /> */}
+      <Login />
     </div>
   )
 }
