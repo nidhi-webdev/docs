5f175e7cf9d3cee526a79a1c7bdaf3ad115874b7
nidhi <kumarinidhi0594@gmail.com>
2025-10-25 12:43:47 +0200

    Auth.js

--- docs-backend/routes/auth.js
+++ docs-backend/routes/auth.js
@@ -7,3 +7,18 @@ const users = [
   {id: 1, username: 'admin', password: '123'}
 ];
 
+// Post api 
+router.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    if(username || password ) {
+        return res.status(400).json({success: false, message: 'Username and Password required'});
+    }
+
+    const user = users.find(u => u.username === username && u.password === password);
+    if(!user) {
+        return res.status(401).json({ success: false, message: 'Invalid Credentials'});
+    }
+
+    
+})
+
