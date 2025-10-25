cc1b96d4bba4f48c682bdacb840f7f493cffb6f2
nidhi <kumarinidhi0594@gmail.com>
2025-10-25 13:03:12 +0200

    trying to access the backend

--- docs-backend/server.js
+++ docs-backend/server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mount auth routes
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+
 // Test route
 app.get('/', (req, res) => {
     res.send('Backend is running');
