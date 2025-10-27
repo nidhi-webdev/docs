07df843b22227e263991c7000d282d53a69dbaa5
nidhi <kumarinidhi0594@gmail.com>
2025-10-27 17:57:06 +0100

    First we will use UseEffect to set the items

--- docs-frontend/src/App.jsx
+++ docs-frontend/src/App.jsx
@@ -2,10 +2,15 @@
 import BackGround from './Components/backGround'
 import ForGround from './Components/ForGround'
 import Login from './Components/login'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState()
+
+  useEffect(() => {
+    
+  }, [third])
+  
 
 if(!isLoggedIn) {
   return <Login onLoginSuccess = {() => setIsLoggedIn(true) } />
