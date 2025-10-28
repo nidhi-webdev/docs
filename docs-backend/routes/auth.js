141da85adcdf3f864117af8a5b51dadddb948873
nidhi <kumarinidhi0594@gmail.com>
2025-10-28 19:37:14 +0100

    Backend JWT_SECRET

--- docs-backend/routes/auth.js
+++ docs-backend/routes/auth.js
@@ -7,23 +7,33 @@ const JWT_SECRET = 'docManagementSystem2024SecretKey!@#$%';
 
 // Temporary HardCorded User 
 const users = [
-  {id: 1, username: 'admin', password: '123', email: 'admin@example.com'}
+    { id: 1, username: 'admin', password: '123', email: 'admin@example.com' }
 ];
 
 // Post api 
 router.post('/login', (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
-    if(!username || !password ) {
-        return res.status(400).json({success: false, message: 'Username and Password required'});
+    if (!username || !password) {
+        return res.status(400).json({ success: false, message: 'Username and Password required' });
     }
 
     const user = users.find(u => u.username === username && u.password === password);
-    if(!user) {
-        return res.status(401).json({ success: false, message: 'Invalid Credentials'});
+    if (!user) {
+        return res.status(401).json({ success: false, message: 'Invalid Credentials' });
     }
 
-    res.json({ success: true, message: 'Login successful!', user: {id: user.id, username: user.username}});
+    // Creating JWT TOKEN
+    const token = jwt.sign(
+        {
+            userId: user.id,
+            username: user.username,
+            email: user.email
+        },
+        JWT_SECRET,
+        { expiresIn: '7d' }
+    );
+
 });
 
 module.exports = router;
