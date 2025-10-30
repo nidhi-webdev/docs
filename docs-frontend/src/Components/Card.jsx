bc078f440696d73307f58ddde924ad45817361ef
nidhi <kumarinidhi0594@gmail.com>
2025-10-30 21:20:43 +0100

    trying to add fileUpload functionality

--- docs-frontend/src/Components/Card.jsx
+++ docs-frontend/src/Components/Card.jsx
@@ -22,6 +22,10 @@ const Card = ({ data = {}, reference, onDelete, onEdit }) => {
     
     const tag = data.tag || {}
 
+    const handleUploadClick = () => {
+        
+    }
+
     return (
         <motion.div drag dragConstraints={reference} whileDrag={{ scale: 1.2 }} dragElastic={0.1} className='w-60 h-72 bg-zinc-900 rounded-[45px] px-8 py-10 relative text-white overflow-hidden cursor-pointer'>
             <div className='absolute top-3 flex gap-1'>
@@ -42,7 +46,8 @@ const Card = ({ data = {}, reference, onDelete, onEdit }) => {
                 <div className='footer bottom-0 absolute w-full left-0'>
                     <div className='flex items-center justify-between mb-3 px-8 py-3'>
                         <h5> {data.filesize} </h5>
-                        <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
+                        <span onClick={handleUploadClick}
+                        className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
                             {data.close ? <IoMdCloseCircle /> : <FaCloudUploadAlt size=".8em" color='#fff' />}
                         </span>
                     </div>
