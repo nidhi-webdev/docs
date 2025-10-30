8c62a54aab2ddb493f382a046ae35e20922e0e31
nidhi <kumarinidhi0594@gmail.com>
2025-10-30 20:04:06 +0100

    trying to add fileUpload functionality

--- docs-frontend/src/Components/FileUpload.jsx
+++ docs-frontend/src/Components/FileUpload.jsx
@@ -1,6 +1,6 @@
+import { useState } from "react"
 
-
-const FileUpload = () => {
+const FileUpload = ({ onFileSelected }) => {
     const [files, setFiles] = useState([])
 
    const handleChange = (e) => {
