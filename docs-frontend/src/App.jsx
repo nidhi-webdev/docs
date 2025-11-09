e4b28211c7c9d87932d91116f740c31b532f19cb
nidhi <kumarinidhi0594@gmail.com>
2025-11-09 15:24:18 +0100

    doing the things in App file

--- docs-frontend/src/App.jsx
+++ docs-frontend/src/App.jsx
@@ -5,13 +5,15 @@ import Login from './Components/login'
 import { useEffect, useState } from 'react'
 
 const App = () => {
-  const [username, setUsername] = useState(() => localStorage.getItem('username' || '' ))
+  // const [username, setUsername] = useState(() => localStorage.getItem('username' || '' ))
+  const [username, setUsername] = useState(() => localStorage.getItem('username') || '')
+
 
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
 
    const token = localStorage.getItem('authToken')
    const loginStatus = localStorage.getItem('isLoggedIn')
-   return token && loginStatus === 'true'
+   return !!token && loginStatus === 'true'
   })
 
   useEffect(() => {
@@ -20,15 +22,7 @@ const App = () => {
   
 
 if(!isLoggedIn) {
-  return <Login onLoginSuccess = {(loggedInUsername, token) =>  {
-    if(token) localStorage.setItem('authToken', token)
-      if(loggedInUsername) {
-        localStorage.setItem('username', loggedInUsername)
-        setUsername(loggedInUsername)
-      }
-      setIsLoggedIn(true) }
-  }
-     />
+ 
 }
 
   return (
